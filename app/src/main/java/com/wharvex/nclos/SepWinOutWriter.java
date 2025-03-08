09991b736105f38708981760375f7be5ddc4635a
Tim <81175852+wharvex@users.noreply.github.com>
2025-03-08 00:11:09 -0500

    customize log window look and feel

--- app/src/main/java/com/wharvex/nclos/SepWinOutWriter.java
+++ app/src/main/java/com/wharvex/nclos/SepWinOutWriter.java
@@ -1,14 +1,14 @@
 package com.wharvex.nclos;
 
-import java.awt.BorderLayout;
+import com.formdev.flatlaf.FlatDarkLaf;
+
+import java.awt.*;
 import java.io.OutputStream;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 import java.util.logging.StreamHandler;
 
-import javax.swing.JFrame;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
+import javax.swing.*;
 
 /**
  * Creates a separate Swing window to display log messages.
@@ -18,29 +18,40 @@ public class SepWinOutWriter {
     private static SepWinOutWriter instance;
 
     private SepWinOutWriter() {
+        // Set up the FlatLaf look and feel.
+        FlatDarkLaf.setup();
+//        UIManager.put("TitlePane.background", Color.BLACK);
+        UIManager.put("TitlePane.foreground", Color.RED);
+
+        logger = Logger.getLogger("MyLog");
+
+//        SwingUtilities.invokeLater(() -> {
         // Set up the Swing window.
         var frame = new JFrame("Log Window");
         var textArea = new JTextArea();
+        Font newFont = new Font("Courier New", Font.PLAIN, 16);
+        textArea.setFont(newFont);
+        textArea.setBackground(new Color(0x141414));
+        textArea.setForeground(Color.LIGHT_GRAY);
         var scrollPane = new JScrollPane(textArea);
         frame.add(scrollPane, BorderLayout.CENTER);
-        frame.setSize(800, 400);
+        frame.setSize(1111, 400);
 
-        // Create the logger.
-        logger = Logger.getLogger("MyLog");
-
-        // We don't want to print to the console.
-        logger.setUseParentHandlers(false);
-
+        // Set up the logger.
+        logger.setUseParentHandlers(false); // Don't print to console.
         var handler = new StreamHandler(new OutputStream() {
             @Override
             public void write(int b) {
+                // Use the Swing window's text area to display log messages.
                 textArea.append(String.valueOf((char) b));
                 textArea.setCaretPosition(textArea.getDocument().getLength());
             }
         }, new SimpleFormatter());
-
         logger.addHandler(handler);
+
+        // Display the Swing window.
         frame.setVisible(true);
+//        });
     }
 
     public static SepWinOutWriter getInstance() {
