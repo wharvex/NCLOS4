93354e6c429340e09666039c527874f0bdab91bb
Tim <81175852+wharvex@users.noreply.github.com>
2025-03-11 11:06:44 -0400

    fix pcb, ping, and pong

--- app/src/main/java/com/wharvex/nclos/PCB.java
+++ app/src/main/java/com/wharvex/nclos/PCB.java
@@ -4,178 +4,184 @@ import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.logging.Level;
 import java.util.stream.IntStream;
 
 public class PCB {
-    private static int nextPid = 0;
-    private final int[] deviceContentsIds;
-    private final VirtualToPhysicalMapping[] v2ps;
-    private final UserlandProcess userlandProcess;
-    private final int pid;
-    private final int[] memoryMap = new int[OS.getMemoryMapSize()];
-    private List<KernelMessage> messages;
-    private Scheduler.PriorityType priorityType;
-    // The Instant before which we should not wake up this PCB if it is sleeping.
-    private Instant wakeupAfter;
-    // How many times the Timer has stopped this PCB.
-    private int timeoutsCounter;
+  private static int nextPid = 0;
+  private final int[] deviceContentsIds;
+  private final VirtualToPhysicalMapping[] v2ps;
+  private final UserlandProcess userlandProcess;
+  private final int pid;
+  private final int[] memoryMap = new int[OS.getMemoryMapSize()];
+  private List<KernelMessage> messages;
+  private Scheduler.PriorityType priorityType;
+  // The Instant before which we won't wake up this PCB if it is sleeping.
+  private Instant wakeupAfter;
+  // How many times the Timer has stopped this PCB.
+  private int timeoutsCounter;
 
-    public PCB(UserlandProcess up, Scheduler.PriorityType pt) {
-        userlandProcess = up;
-        priorityType = pt;
+  public PCB(UserlandProcess up, Scheduler.PriorityType pt) {
+    userlandProcess = up;
+    priorityType = pt;
 
-        // For a new PCB, we set pid to the current nextPid and then increment nextPid for the next PCB.
-        pid = PCB.nextPid++;
+    // For a new PCB, we set pid to the current nextPid and then increment
+    // nextPid for the next PCB.
+    pid = PCB.nextPid++;
 
-        timeoutsCounter = 0;
-        messages = new ArrayList<>();
-        IntStream.range(0, OS.getMemoryMapSize()).forEach(i -> getMemoryMap()[i] = -1);
-        deviceContentsIds = MiscHelper.makeIntArr(OS.DEVICE_CONTENTS_SIZE);
-        v2ps = new VirtualToPhysicalMapping[OS.DEVICE_CONTENTS_SIZE];
+    timeoutsCounter = 0;
+    messages = new ArrayList<>();
+    IntStream.range(0, OS.getMemoryMapSize())
+        .forEach(i -> getMemoryMap()[i] = -1);
+    deviceContentsIds = MiscHelper.makeIntArr(OS.DEVICE_CONTENTS_SIZE);
+    v2ps = new VirtualToPhysicalMapping[OS.DEVICE_CONTENTS_SIZE];
+  }
+
+  public VirtualToPhysicalMapping[] getV2ps() {
+    return v2ps;
+  }
+
+  public VirtualToPhysicalMapping getFromV2ps(int idx) {
+    return getV2ps()[idx];
+  }
+
+  public void addToV2ps(int idx, VirtualToPhysicalMapping v2p) {
+    getV2ps()[idx] = v2p;
+  }
+
+  public int[] getDeviceContentsIds() {
+    return deviceContentsIds;
+  }
+
+  public int getFromDeviceContentsIds(int idx) {
+    return getDeviceContentsIds()[idx];
+  }
+
+  public int addToDeviceContentsIds(int id) {
+    int idx = MiscHelper.findNegativeIndex(this::getFromDeviceContentsIds,
+        OS.DEVICE_CONTENTS_SIZE);
+    if (idx < 0) {
+      return idx;
     }
+    getDeviceContentsIds()[idx] = id;
+    return idx;
+  }
 
-    public VirtualToPhysicalMapping[] getV2ps() {
-        return v2ps;
-    }
+  public int[] getMemoryMap() {
+    return memoryMap;
+  }
 
-    public VirtualToPhysicalMapping getFromV2ps(int idx) {
-        return getV2ps()[idx];
-    }
+  public int getTimeoutsCounter() {
+    return timeoutsCounter;
+  }
 
-    public void addToV2ps(int idx, VirtualToPhysicalMapping v2p) {
-        getV2ps()[idx] = v2p;
-    }
+  public void setTimeoutsCounter(int timeoutsCounter) {
+    this.timeoutsCounter = timeoutsCounter;
+  }
 
-    public int[] getDeviceContentsIds() {
-        return deviceContentsIds;
-    }
+  public PCB addAllToMessagesAndReturnThis(List<KernelMessage> kms) {
+    getMessages().addAll(kms);
+    return this;
+  }
 
-    public int getFromDeviceContentsIds(int idx) {
-        return getDeviceContentsIds()[idx];
-    }
+  public String getThreadName() {
+    return getUserlandProcess().getThreadName();
+  }
 
-    public int addToDeviceContentsIds(int id) {
-        int idx = MiscHelper.findNegativeIndex(this::getFromDeviceContentsIds, OS.DEVICE_CONTENTS_SIZE);
-        if (idx < 0) {
-            return idx;
-        }
-        getDeviceContentsIds()[idx] = id;
-        return idx;
-    }
+  public Thread.State getThreadState() {
+    return getUserlandProcess().getThreadState();
+  }
 
-    public int[] getMemoryMap() {
-        return memoryMap;
-    }
+  /**
+   * Priority type can be accessed by timer thread or kernel thread.
+   *
+   * @return
+   */
+  public synchronized Scheduler.PriorityType getPriorityType() {
+    return priorityType;
+  }
 
-    public int getTimeoutsCounter() {
-        return timeoutsCounter;
-    }
+  /**
+   * Priority type can be accessed by timer thread or kernel thread.
+   *
+   * @param priorityType
+   */
+  public synchronized void setPriorityType(
+      Scheduler.PriorityType priorityType) {
+    this.priorityType = priorityType;
+  }
 
-    public void setTimeoutsCounter(int timeoutsCounter) {
-        this.timeoutsCounter = timeoutsCounter;
-    }
+  public UserlandProcess getUserlandProcess() {
+    return userlandProcess;
+  }
 
-    public PCB addAllToMessagesAndReturnThis(List<KernelMessage> kms) {
-        getMessages().addAll(kms);
-        return this;
-    }
+  public int getPid() {
+    return pid;
+  }
 
-    public String getThreadName() {
-        return getUserlandProcess().getThreadName();
-    }
+  /**
+   * Only the Timer thread should use this method.
+   */
+  public void stop() {
+    getUserlandProcess().requestStop();
+    getUserlandProcess().waitUntilStoppedByRequest();
+    OutputHelper.getInstance().getDebugLogger()
+        .log(Level.INFO, "Stopped " + getThreadName());
+    incrementTimeoutsCounter();
+  }
 
-    public Thread.State getThreadState() {
-        return getUserlandProcess().getThreadState();
+  /**
+   * Only called by timer thread.
+   */
+  private void incrementTimeoutsCounter() {
+    setTimeoutsCounter(getTimeoutsCounter() + 1);
+    if (getTimeoutsCounter() > 4) {
+      demote();
     }
+  }
 
-    /**
-     * Priority type can be accessed by timer thread or kernel thread.
-     *
-     * @return
-     */
-    public synchronized Scheduler.PriorityType getPriorityType() {
-        return priorityType;
-    }
+  public boolean isDone() {
+    return getUserlandProcess().isDone();
+  }
 
-    /**
-     * Priority type can be accessed by timer thread or kernel thread.
-     *
-     * @param priorityType
-     */
-    public synchronized void setPriorityType(Scheduler.PriorityType priorityType) {
-        this.priorityType = priorityType;
+  private void demote() {
+    if (getPriorityType() == Scheduler.PriorityType.REALTIME) {
+      setPriorityType(Scheduler.PriorityType.INTERACTIVE);
+    } else if (getPriorityType() == Scheduler.PriorityType.INTERACTIVE) {
+      setPriorityType(Scheduler.PriorityType.BACKGROUND);
     }
+  }
 
-    public UserlandProcess getUserlandProcess() {
-        return userlandProcess;
-    }
+  public void init() {
+    getUserlandProcess().init();
+  }
 
-    public int getPid() {
-        return pid;
-    }
+  public void start() {
+    getUserlandProcess().start();
+  }
 
-    /**
-     * Only the Timer thread should use this method.
-     */
-    public void stop() {
-        getUserlandProcess().requestStop();
-        getUserlandProcess().waitUntilStoppedByRequest();
-        OutputHelper.debugPrint("Successfully stopped " + getThreadName());
-        incrementTimeoutsCounter();
-    }
+  /**
+   * TODO: What does it mean if this is null?
+   *
+   * @return
+   */
+  public Optional<Instant> getWakeupAfter() {
+    return Optional.ofNullable(wakeupAfter);
+  }
 
-    /**
-     * Only called by timer thread.
-     */
-    private void incrementTimeoutsCounter() {
-        setTimeoutsCounter(getTimeoutsCounter() + 1);
-        if (getTimeoutsCounter() > 4) {
-            demote();
-        }
-    }
+  public void setWakeupAfter(Instant wakeupAfter) {
+    this.wakeupAfter = wakeupAfter;
+  }
 
-    public boolean isDone() {
-        return getUserlandProcess().isDone();
-    }
+  public void resetTimeoutsCounter() {
+    setTimeoutsCounter(0);
+  }
 
-    private void demote() {
-        if (getPriorityType() == Scheduler.PriorityType.REALTIME) {
-            setPriorityType(Scheduler.PriorityType.INTERACTIVE);
-        } else if (getPriorityType() == Scheduler.PriorityType.INTERACTIVE) {
-            setPriorityType(Scheduler.PriorityType.BACKGROUND);
-        }
-    }
+  public List<KernelMessage> getMessages() {
+    return messages;
+  }
 
-    public void init() {
-        getUserlandProcess().init();
-    }
-
-    public void start() {
-        getUserlandProcess().start();
-    }
-
-    /**
-     * TODO: What does it mean if this is null?
-     *
-     * @return
-     */
-    public Optional<Instant> getWakeupAfter() {
-        return Optional.ofNullable(wakeupAfter);
-    }
-
-    public void setWakeupAfter(Instant wakeupAfter) {
-        this.wakeupAfter = wakeupAfter;
-    }
-
-    public void resetTimeoutsCounter() {
-        setTimeoutsCounter(0);
-    }
-
-    public List<KernelMessage> getMessages() {
-        return messages;
-    }
-
-    public void setMessages(List<KernelMessage> messages) {
-        this.messages = messages;
-    }
+  public void setMessages(List<KernelMessage> messages) {
+    this.messages = messages;
+  }
 }
